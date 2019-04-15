aa92b2b81133c5455565f6d78e907a2275952f04
mj <onyangomaureen95@gmail.com>
2019-04-15 09:48:47 +0300

    add appbar and texts color features

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(StatelessApp());
 }
 
+class StatelessApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      theme: ThemeData(primaryColor: Colors.white),
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text(
+              'Stateless App',
+              style: TextStyle(
+                color: Colors.green,
+              ),
+            ),
+            centerTitle: true),
+      ),
+    );
+  }
+}
